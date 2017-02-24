bf27100bd0479a4f4f5a2519a4d3cf4b3c6b8d0f
Stephan Baker <stephan.baker@gmail.com>
2017-02-24 00:56:45 -0500

    Fixing jitter caused by camera rotations.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 function main() {
     // Setup our scene
     var scene = new THREE.Scene()
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.z = 20
     var renderer = new THREE.WebGLRenderer()
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild( renderer.domElement )
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
     // Draw our line
     var material = new THREE.LineBasicMaterial({ color: 0xBADA55 })
     var geometry = new THREE.Geometry()
-    var lineVector1 = new THREE.Vector3( 0, -10, 0 )
-    var lineVector2 = new THREE.Vector3( 0, 10, 0 )
+    var lineVector1 = new THREE.Vector3(0, -10, 0)
+    var lineVector2 = new THREE.Vector3(0, 10, 0)
     geometry.vertices.push(lineVector1, lineVector2)
-    var line = new THREE.Line( geometry, material )
-    scene.add( line )
+    var line = new THREE.Line(geometry, material)
+    scene.add(line)
 
-    //First pass to generate points and calculate the closest
+    // Create a parent for the points
+    pointsContainer = new THREE.Object3D()
+    scene.add(pointsContainer)
+
+    // First pass to generate points and calculate the closest
     var points = []
     var closestIndex = 0
     var closestDistance
@@ -36,24 +40,24 @@ function main() {
         points.push(point)
     }
 
-    //Second pass to add our points and visually distinguish the closest
+    // Second pass to add our points and visually distinguish the closest
     for(var i=0;i<points.length; i++) {
         var mat = new THREE.MeshBasicMaterial({
             color: (i == closestIndex) ? 0xBADA55 : 0xCCCCCC
         })
         var point = points[i]
-        var sphereGeometry = new THREE.SphereGeometry( 0.2, 20, 20, 0, Math.PI * 2, 0, Math.PI * 2)
+        var sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32)
         var sphere = new THREE.Mesh(sphereGeometry, mat)
         sphere.position.set(point.x, point.y, point.z)
-        scene.add(sphere)
+        pointsContainer.add(sphere)
     }
 
     var cameraRotation = 0
     function render() {
         requestAnimationFrame( render )
-        cameraRotation += 1
-        rotateCamera(camera, cameraRotation)
-        renderer.render( scene, camera )
+        cameraRotation -= 0.01
+        pointsContainer.rotation.y = cameraRotation
+        renderer.render(scene, camera)
     }
     render()
 }
@@ -73,13 +77,6 @@ function calculateDistance(v, l1, lv2) {
     var cross = new THREE.Vector3(0,0,0).crossVectors(diff1, diff2)
     var diff3 = new THREE.Vector3(0,0,0).subVectors(lv2, l1)
     return cross.length() / diff3.length()
-}
-
-function rotateCamera(camera, degrees) {
-    rotation = degreesToRadians(degrees)
-    camera.position.x = Math.sin(rotation) * 20.0
-    camera.position.z = Math.cos(rotation) * 20.0
-    camera.lookAt(new THREE.Vector3(0,0,0)) 
 }
 
 main()
